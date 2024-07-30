2b0c6fc1b940c24704602d369c5d129594c3a5b4
Ludmila Brus <luntalumi@yandex.ru>
2024-07-30 15:13:47 +0300

    add collection

--- src/Practicum.java
+++ src/Practicum.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Collection; // импортируем интерфейс Collection
+
+
 
 public class Practicum {
 
@@ -16,5 +19,18 @@ public class Practicum {
             final String element = iterator.next();  // возвращаем элемент
             System.out.print(element);
         }
+
+        final Collection<String> colors = new ArrayList<>();
+        colors.add("Жёлтый");
+        colors.add("Красный");
+        colors.add("Зелёный");
+
+        System.out.println("Список содержит Зелёный: " + colors.contains("Зелёный"));
+
+        colors.remove("Жёлтый");
+        System.out.println(colors);
+
+        System.out.println("Оставшееся количество элементов: " + colors.size());
+        System.out.println("Коллекция пустая? " + colors.isEmpty());
     }
 }
