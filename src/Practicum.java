c7e7a80873c0d6d54256887f4716e3213c072e94
Ludmila Brus <luntalumi@yandex.ru>
2024-07-30 15:05:09 +0300

    first

---
+++ src/Practicum.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Practicum {
+
+    public static void main(String[] args) {
+        final ArrayList<String> list = new ArrayList<>();
+        list.add("Hello");
+        list.add(" ");
+        list.add("World!");
+
+        final Iterator<String> iterator; // объявили объект iterator
+        iterator = list.iterator(); // проинициализировали iterator через метод iterator()
+
+        while (iterator.hasNext()) { // проверяем, есть ли следующий элемент в коллекции
+            final String element = iterator.next();  // возвращаем элемент
+            System.out.print(element);
+        }
+    }
+}
