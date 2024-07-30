7db23e01d4e46f027200e3f1c5d8e13afb70f4b5
Ludmila Brus <luntalumi@yandex.ru>
2024-07-30 15:20:45 +0300

    work with all

--- src/Practicum.java
+++ src/Practicum.java
@@ -32,5 +32,31 @@ public class Practicum {
 
         System.out.println("Оставшееся количество элементов: " + colors.size());
         System.out.println("Коллекция пустая? " + colors.isEmpty());
+
+        workWithAll();
+    }
+
+    public static void workWithAll() {
+        final Collection<String> colors = new ArrayList<>();
+        colors.add("Жёлтый");
+        colors.add("Красный");
+
+        final Collection<String> colorsAdditional = new ArrayList<>();
+        colorsAdditional.add("Зелёный");
+        colorsAdditional.add("Жёлтый");
+
+        colors.addAll(colorsAdditional);
+        System.out.println(colors);
+
+        final Collection<String> greenAndRed = new ArrayList<>();
+        greenAndRed.add("Зелёный");
+        greenAndRed.add("Красный");
+        System.out.println("Список содержит Зелёный и Красный: " + colors.containsAll(greenAndRed));
+
+        final Collection<String> yellowOnly = new ArrayList<>();
+        yellowOnly.add("Жёлтый");
+        colors.removeAll(yellowOnly);
+
+        System.out.println(colors);
     }
 }
